9fdd942ebf6bf6828c9fa376a78c9881ece19e3e
laetitiamoyon <laetitia.moyon.pro@gmail.com>
2021-07-19 19:12:21 +0200

    wip

--- netflixvf/index.js
+++
@@ -1,6 +0,0 @@
-$(".faq-item").click(function()
-{
-    $(this).children().next('.faq-question-expand-image').toggle();
-    $(this).children().next('.faq-question-close-image').toggle();
-    $(this).parent().find('.faq-answer').toggle();
-});
