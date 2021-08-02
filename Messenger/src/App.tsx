8c2890acf267fb34fca0ff85b7c531312abe047b
laetitiamoyon <laetitia.moyon.pro@gmail.com>
2021-08-02 16:25:19 +0200

    push

--- Messenger/src/App.tsx
+++ Messenger/src/App.tsx
@@ -4,30 +4,30 @@ import Send from './images/send.svg'
 
 const App = () => 
 {
-    // Pas les m^mes arrondies sur les message
-    // Texte gris si c'est from other user
+    // Vérifier l'ordre
     // Responsive, tester sur un petit écran style iphone
     return <>
         <div className="container">
-            <div className="username-container">
+            <div className="usernameContainer">
                 <div className="title">Scouting Group</div>
-                <input className="username" placeholder="Username"/>
+                <input className="input" placeholder="Username"/>
             </div>
 
-            <div className="description-container">
-                <div className="title-description">Welcome to the Streamline scouting chat</div>
-                <div className="text-description">We can now freely colaborate regarding our current 
+            <div className="descriptionContainer">
+                <div className="titleDescription">Welcome to the Streamline scouting chat</div>
+                <div className="description">We can now freely colaborate regarding our current 
                 Demand. Any question about the documentation or the project, please feel free to get
                 in contact us.
                 </div>
             </div>
-            <div className="date-message">Tuesday, April 7th at 1:21 PM</div>
+
+            <div className="messageDate">Tuesday, April 7th at 1:21 PM</div>
 
             <div className="messageFromOtherUser">
                 <div className="messageTitle">Jess sent a message to the group</div> 
                 <div className="conversationContainer">
                     <img className="profilImage"/>
-                    <div className="message-container">
+                    <div>
                         <div className="message">Awesome!It's going to amazing deal!</div>
                         <div className="message">I've ran through different docs</div>
                         <div className="message">Hope for the best</div>
@@ -39,7 +39,7 @@ const App = () =>
                 <div className="messageTitle">You sent a message to everybody</div> 
                 <div className="conversationContainer">
                     <img className="profilImage"/>
-                    <div className="message-container">
+                    <div>
                         <div className="message">Thanks you for sending the deal, I'll review it
                         and get come back to you shortly</div>
                     </div>
@@ -50,7 +50,7 @@ const App = () =>
                 <div className="messageTitle">Jess sent a message to the group</div> 
                 <div className="conversationContainer">
                     <img className="profilImage"/>
-                    <div className="message-container">
+                    <div>
                         <div className="message">Awesome!It's going to amazing deal!</div>
                     </div>
                 </div>
@@ -60,36 +60,29 @@ const App = () =>
                 <div className="messageTitle">You sent a message to everybody</div> 
                 <div className="conversationContainer">
                     <img className="profilImage"/>
-                    <div className="message-container">
+                    <div>
                         <div className="message">Leo, can you provide the lastest doc ?</div>
                     </div>
                 </div>
             </div>
 
-            <div className="message-to-everybody-container">
-                <input className="text-area"
-                placeholder="Write a message..."/>
-                <div className="send-button"></div>
+            <div className="messageDestinationInputContainer">
+                <input className="input" placeholder="Write a message..."/>
                 <img src={Send}/>
             </div>
 
-            <div className="group-message-container">
-                <input className="text-area-group"
-                placeholder="Write a message..."/>
-                <input className="group-name"
-                placeholder="Group Name"/>
+            <div className="messageDestinationInputContainer">
+                <input className="input" placeholder="Write a message..."/>
+                <input className="input" placeholder="Group Name"/>
                 <img src={Join}/>
                 <img src={Send}/>
             </div>
 
-            <div className="private-message-container">
-                <input className="text-area-private"
-                placeholder="Write a message..."/>
-                <input className="username"
-                placeholder="Username"/>
+            <div className="messageDestinationInputContainer">
+                <input className="input" placeholder="Write a message..."/>
+                <input className="input" placeholder="Username"/>
                 <img src={Send}/>
             </div>
-
         </div>
     </>
 }
